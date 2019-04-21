e1c13b28aee00b0d9d6209348f9b1759f961a4e4
fatma arik copur <fatmaarik@gmail.com>
2019-04-21 15:33:47 -0400

    category controller is developed.

---
+++ src/main/java/com/orionsson/spring5webfluxrest/controllers/CategoryController.java
@@ -0,0 +1,28 @@
+package com.orionsson.spring5webfluxrest.controllers;
+
+import com.orionsson.spring5webfluxrest.domain.Category;
+import com.orionsson.spring5webfluxrest.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> list(){
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getById(@PathVariable String id){
+        return categoryRepository.findById(id);
+    }
+}
