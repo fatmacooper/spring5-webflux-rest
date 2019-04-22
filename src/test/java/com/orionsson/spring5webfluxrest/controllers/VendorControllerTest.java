dfee0d77bc15148ef632bfd8ac2d740ebd96219d
fatma arik copur <fatmaarik@gmail.com>
2019-04-22 05:28:34 -0400

    vendor controller is developed.

---
+++ src/test/java/com/orionsson/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,50 @@
+package com.orionsson.spring5webfluxrest.controllers;
+
+import com.orionsson.spring5webfluxrest.domain.Vendor;
+import com.orionsson.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+
+public class VendorControllerTest {
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("ftm").lastName("cpr").build(),
+                            Vendor.builder().firstName("rcp").lastName("cpr").build()));
+        webTestClient
+                .get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(vendorRepository.findById("someid"))
+                .willReturn(Mono.just(Vendor.builder().firstName("rcp").lastName("cpr").build()));
+        webTestClient
+                .get()
+                .uri("/api/v1/vendors/someid")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
